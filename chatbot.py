3d440aa6adaedc3263c200196a39cbe382e02c83
Daniel <60708417+danielsandorfer@users.noreply.github.com>
2022-01-17 21:15:50 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,130 +1,144 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 import json
 import pickle
-
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SGD
+from keras.layers import Dense, Activation, Dropout, Embedding, LSTM, Bidirectional, Input, concatenate
+from tensorflow.keras.optimizers import SGD, Adam
 import random
+from nltk.corpus import stopwords
+import string
+import re
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+nltk.download('punkt')
+nltk.download('stopwords')
 
 class Chatbot:
-    def __init__(self, error_threshold=0.35):
-        # list of tokenized words
-        self.vocabulary = []
+    def __init__(self, error_threshold=0.3, tokenizer_maxlen=35, embedding_size=25):
         # possible tags for sentences (e.g. greeting)
         self.classes = []
-        # lists of patterns for specific tags for training
-        self.documents = []
-        self.ignore_words = ['?', '!']
         # mapping of responses for patterns by tag (class)
         self.intents = json.loads(open('intents.json').read())
         self.model = Sequential()
         self.ERROR_THRESHOLD = error_threshold
+        self.lemmatizer = WordNetLemmatizer()
+        self.exclude_words = []
+        self.tokenizer = Tokenizer()
+        self.vocabulary_size = 0
+        self.tokenizer_maxlen = tokenizer_maxlen
+        self.embedding_size = embedding_size
         
-    def create_helper_files(self):
-        for intent in self.intents['intents']:
-            for pattern in intent['patterns']:
-                # tokenize sentences
-                words = nltk.word_tokenize(pattern)
-                voc_words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in self.ignore_words]
-                self.vocabulary.extend(voc_words)
-                # add documents in the corpus
-                self.documents.append((words, intent['tag']))
-                # add to our classes set
-                if intent['tag'] not in self.classes:
-                    self.classes.append(intent['tag'])
-
-        # lemmaztize and lower each word and remove duplicates
-        self.vocabulary = sorted(list(set(self.vocabulary)))
-        # sort classes
-        self.classes = sorted(list(set(self.classes)))
-       
-    def create_training_data(self):
-         # create classes and vocabulary
-        self.create_helper_files()
-        # create our training data
-        training = []
-        # create an empty array for our output
-        output_empty = [0] * len(self.classes)
-        # training set, bag of words for each sentence
-        for doc in self.documents:
-            # initialize our bag of words
-            bag = []
-            # list of tokenized words for the pattern
-            pattern_words = doc[0]
-            # lemmatize each word - create base word, in attempt to represent related words
-            pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-            # create our bag of words array with 1, if word match found in current pattern
-            for word in self.vocabulary:
-                bag.append(1) if word in pattern_words else bag.append(0)
-
-            # output is a '0' for each tag and '1' for current tag (for each pattern)
-            output_row = list(output_empty)
-            output_row[self.classes.index(doc[1])] = 1
-            training.append([bag, output_row])
-            
-        # shuffle our features and turn into np.array
-        random.shuffle(training)
-        training = np.array(training)
-        # create train and test lists. X - patterns, Y - intents
-        train_x = list(training[:,0])
-        train_y = list(training[:,1])
-        print(f"Number of classes: {len(self.classes)}")
-        print(f"Vocabulary size: {len(self.vocabulary)}")
-        print(f"Training data size: {len(train_x)}")
-        print("Training data created")
-        return train_x, train_y
-        
-    def train(self):
-        # create training data
-        train_x, train_y = self.create_training_data()
-        # initialize model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-        # equal to number of intents to predict output intent with softmax
-        self.model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-        self.model.add(Dropout(0.5))
+    def initialize_model(self):
+        # initialize model - first is the embedding to 24 dimension, then 2 layers of bidirectional LSTMs for context memorization,
+        # then dense layers with dropouts to stop overfitting
+        self.model.add(Embedding(self.vocabulary_size, self.embedding_size, input_length=self.tokenizer_maxlen))
+        self.model.add(Bidirectional(LSTM(36, return_sequences=True)))
+        self.model.add(Bidirectional(LSTM(24)))
         self.model.add(Dense(64, activation='relu'))
         self.model.add(Dropout(0.5))
-        self.model.add(Dense(len(train_y[0]), activation='softmax'))
-
-        # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-        sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-        self.model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-
+        self.model.add(Dense(len(self.classes), activation='softmax'))
+        # Compile model
+        adam = Adam(learning_rate=0.01)
+        self.model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+        
+    def decontract(self, phrase):
+        phrase = re.sub(r"\'m", " am", phrase)
+        phrase = re.sub(r"n\'t", " not", phrase)
+        phrase = re.sub(r"\'re", " are", phrase)
+        phrase = re.sub(r"\'s", " is", phrase)
+        phrase = re.sub(r"\'d", " would", phrase)
+        phrase = re.sub(r"\'ll", " will", phrase)
+        phrase = re.sub(r"\'t", " not", phrase)
+        phrase = re.sub(r"\'ve", " have", phrase) 
+        return phrase
+        
+    def clean_pattern(self, pattern):
+        # decontract contracted forms
+        decontractated_pattern = self.decontract(pattern)
+        # remove non-alphanumeric signs
+        non_alpha_pattern = re.sub('[^A-Za-z0-9]+', ' ', decontractated_pattern)
+        # tokenize sentences
+        words = nltk.word_tokenize(non_alpha_pattern)
+        voc_words = [self.lemmatizer.lemmatize(w.lower()) for w in words if w not in self.exclude_words]
+        return ' '.join(voc_words)
+        
+    def create_helper_files(self):
+        # lists of training data and labels
+        x_train = []
+        y_train = []
+        # exclude stop words ("the", "a", etc.) and punctuation
+        stop_words = set(stopwords.words('english'))
+        for character in set(string.punctuation):
+            self.exclude_words.append(character)
+        self.exclude_words.extend(list(stop_words))
+        # intents is a list of objects containing patterns (questions) and responses (possible answers)
+        for intent in self.intents['intents']:
+            for pattern in intent['patterns']:
+                cleaned_pattern = self.clean_pattern(pattern)
+                # add documents in the corpus to the training set
+                x_train.append(cleaned_pattern)
+                y_train.append(intent['tag'])
+                # add names of classes (e.g., greeting)
+                if intent['tag'] not in self.classes:
+                    self.classes.append(intent['tag'])
+        # sort classes
+        self.classes = sorted(list(set(self.classes)))
+        return x_train, y_train
+       
+    def format_training_data(self):
+         # create classes and vocabulary
+        x_train, y_train = self.create_helper_files()
+        # create our training data, text data is tokenized
+        self.tokenizer.fit_on_texts(x_train)
+        training_sequences = self.tokenizer.texts_to_sequences(x_train)
+        # all rows padded to size of 50
+        training_data = pad_sequences(training_sequences, maxlen=self.tokenizer_maxlen, padding='post', truncating='post')
+        self.vocabulary_size = len(self.tokenizer.word_index) + 1
+        # create labels for training data
+        training_labels = []
+        # create an empty array for our output
+        output_empty = [0] * len(self.classes)
+        # form one-hot encoding of the label
+        for tag in y_train:
+            output_row = list(output_empty)
+            output_row[self.classes.index(tag)] = 1
+            # training data contains the one-hot encoding for the class (tag)
+            training_labels.append(output_row)           
+        # shuffle the texts and labels and turn into numpy arrays
+        combined = list(zip(training_data, training_labels))
+        random.shuffle(combined)
+        training_data, training_labels = zip(*combined)
+        training_data = np.array(training_data)
+        training_labels = np.array(training_labels)
+        print(f"Number of classes: {len(self.classes)}")
+        print(f"Vocabulary size: {self.vocabulary_size}")
+        print(f"Training data size: {len(training_data)}")
+        print("Training data created")
+        self.initialize_model()
+        print("Model initialized")
+        return training_data, training_labels
+        
+    def train(self, epochs=30, batch_size=5, verbose=1):
+        x_train, y_train = self.format_training_data() 
         #fitting and saving the model 
-        hist = self.model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-        self.model.save('chatbot_model.h5', hist)
+        hist = self.model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
+        self.model.save('chatbot_model_3.h5', hist)
         print("model created")
         return self
-    
-    def clean_up_sentence(self, sentence):
-        # tokenize the pattern - split words into array
-        sentence_words = nltk.word_tokenize(sentence)
-        # stem each word - create short form for word
-        sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-        return sentence_words
 
-    # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-    def bow(self, sentence):
-        # tokenize the pattern
-        sentence_words = self.clean_up_sentence(sentence)
-        # bag of words - matrix of N words, vocabulary matrix
-        bag = [0] * len(self.vocabulary)  
-        for s in sentence_words:
-            for i,w in enumerate(self.vocabulary):
-                if w == s: 
-                    # assign 1 if current word is in the vocabulary position
-                    bag[i] = 1
-                    break
-        return(np.array(bag))
+    def create_sequence(self, sentence):
+        sequence = self.tokenizer.texts_to_sequences([sentence])
+        # all rows padded to size of 50
+        padded_sequence = pad_sequences(sequence, maxlen=self.tokenizer_maxlen, padding='post', truncating='post')
+        return np.array(padded_sequence)
 
     def predict(self, sentence):
         # filter out predictions below a threshold
-        p = self.bow(sentence)
-        res = self.model.predict(np.array([p]))[0]
-        print(f"Predictions of class for sentance: {sentence}\n")
+        sequence = self.create_sequence(sentence)
+        res = self.model.predict(sequence)[0]
+        print(f"\nPredictions of class for sentance: {sentence}\n")
         for i,r in enumerate(res):
             print(f"intent: {self.classes[i]}, probabilty: {str(r)}\n")
         results = [[i,r] for i,r in enumerate(res) if r > self.ERROR_THRESHOLD]
